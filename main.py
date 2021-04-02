88f210443ac6d72bfe9a34c53ce155a7034d3dd9
Debian <debian@debian10.linuxvmimages.local>
2021-04-02 10:13:34 -0500

    getting closer

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import json
 
-def get_user_data():
-    name = input("Hello there!, What's your name/surname?")
-
-
 def loadExistingUsers():
     with open('user.json') as existing_users:
         users = json.load(existing_users)
@@ -12,9 +8,10 @@ def loadExistingUsers():
 
 def check_for_username(username, users):
     for user in users:
-        s = user["Name_Surname"]
+        s = user["name_surname"]
         if(username == s):
             print(username)
+            return 0
 
 def save_to_file(users):
     f = open("user.json","w")
@@ -22,22 +19,36 @@ def save_to_file(users):
     f.close
 
 def add_user():
-	user = {
-		"name_surname" : "",
-		"age" : 0,
-		"gender" : 0,
-		"starting_weight" : 0,
-		"height" : 0,
-		"fitness_level" : 0,
-		"health_restrictions" : [0],
-		}
-	user["name"] = input("Please insert your name and surname: ")
-	user["age"] = input("Please insert your age: ")
-	user["gender"] = input("Please select your gender")
-	user["starting_weight"] = input("Please insert your current weight: ")
-	user["height"] = input("Please insert your height: ")
-	user["fitness_level"] = input("Please select your fitness level: ")
-	user["health_restrictions"] = input("Please select your health conditions from the list: ")
+        user = {"name_surname" : "",
+                "age" : 0,
+                "gender" : 0,
+                "weight" : {
+                    "starting_weight": 0,
+                    "weight_goal": 0,
+                    "current_weight": 0
+                },
+                "height" : 0,
+                "fitness_level" : 0,
+                "health_restrictions" : [0],
+                }
+        user["name_surname"] = input("Please insert your name and surname: ")
+        user["age"] = input("Please insert your age: ")
+        while(True):
+            user["gender"] = input("Please select your gender F/M): ")
+            if(user["gender"] == "F"):
+                user["gender"] = 0
+                break
+            elif(user["gender"] == "M"):
+                user["gender"] == 1
+                break
+            else:
+                print("please enter a valid gender ")
+
+        user["starting_weight"] = input("Please insert your current weight: ")
+        user["height"] = input("Please insert your height in centimeters: ")
+        user["fitness_level"] = input("Please select your fitness level (1 being the lowest, 3 the highest): ")
+        user["health_restrictions"] = input("Please enter any health restrictions you may have ")
+        return user
 	
 
 def main():
@@ -59,12 +70,22 @@ def main():
         insert_mode = input("Are you a new user? (yes/no)")
         if(insert_mode == "no"):
             username =input("Enter username: ")
-            check_for_username(username, users)
-            break
+            n = check_for_username(username, users)
+
+            if(n == 0):
+                print("Welcome back "+ username)
+                break
+            else:
+                print("Sorry you are not a current user")
+                answer =input("Would you like to register? (yes,no): ")
+
+
         elif(insert_mode == "yes"):
             current_user = add_user()
             users.append(current_user)
+            save_to_file(users)
             break
+
         else:
             print ("please enter a valid input ")
     
