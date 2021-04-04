325dca90328d3167675e2b2bb5fa57748e20a23a
Debian <debian@debian10.linuxvmimages.local>
2021-04-04 10:15:24 -0500

    many changes

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import json
+from random import randint
+
+def loadExercises():
+    with open('data.json') as existing_exe:
+        exercises = json.load(existing_exe)
+        return exercises
+
 
 def loadExistingUsers():
     with open('user.json') as existing_users:
@@ -11,7 +18,7 @@ def check_for_username(username, users):
         s = user["name_surname"]
         if(username == s):
             print(username)
-            return 0
+            return [0, user]
 
 def save_to_file(users):
     f = open("user.json","w")
@@ -44,12 +51,48 @@ def add_user():
             else:
                 print("please enter a valid gender ")
 
-        user["starting_weight"] = input("Please insert your current weight: ")
-        user["height"] = input("Please insert your height in centimeters: ")
-        user["fitness_level"] = input("Please select your fitness level (1 being the lowest, 3 the highest): ")
+        a = int(input("Please insert your current weight: "))
+        user["starting_weight"] = a
+        user["weight"]["current_weight"] = a
+        user["height"] = int(input("Please insert your height in centimeters: "))
+        user["fitness_level"] = int(input("Please select your fitness level (1 being the lowest, 3 the highest): "))
         user["health_restrictions"] = input("Please enter any health restrictions you may have ")
         return user
 	
+
+def calculate_bmi(current_user):
+
+    meter= current_user["height"] / 100
+    meter_squared = meter**2
+    bmi = current_user["weight"]["current_weight"]/meter_squared
+    bmi = round (bmi , 1)
+    if (bmi <= 18.5):
+        bmi_status = "U" #U for underweight
+        print("According to your BMI, it is suggested that you gain more weight\n the exercises recommended to you will aim to bulid muscle. ")
+    elif (bmi >= 25):
+        bmi_status = "O" #O for overweight
+        print("According to your BMI, it is suggested that you lose weight,\n the exercises recommended to you will aim to burn fat. ")
+    elif (bmi < 25 and bmi > 18.5):
+        bmi_status = "N" #N for normal
+        print("According to your BMI, you weight is normal,\n the exercises recommended to you will aim to keep you in shape. ")
+
+    return bmi_status
+
+def recommend_exercise(current_user, exercises):
+    selection = []
+    bmi_status = calculate_bmi(current_user)
+    for exercise in exercises:
+        for letter in exercise["bmi_recommended"]:
+            if(letter == bmi_status):
+                selection.append(exercise)
+
+    l = len(selection)
+    i = randint(0, l)
+    return selection[i]
+
+
+
+
 
 def main():
     print(""" /$$$$$$$  /$$                           /$$                        /$$                              
@@ -65,17 +108,20 @@ def main():
                      \______/                                                                        """)
     users = []
     users = loadExistingUsers()
+    exercises = []
+    exercises = loadExercises()
 
     print("Hello, I am Phy-trainer")
     insert_mode = input("Are you a new user? (yes/no)")
-    n = 1
-    while(n != 0):
+    f = 1
+    while(f != 0):
         if(insert_mode == "no"):
             username =input("Enter username: ")
             n = check_for_username(username, users)
 
-            if(n == 0):
+            if(n[0] == 0):
                 print("Welcome back "+ username)
+                recommend_exercise(n[1], exercises)
                 break
             else:
                 print("Sorry you are not a current user")
@@ -83,7 +129,7 @@ def main():
                 while(True):
                     if(answer == "no"):
                         print("Goodbye")
-                        n = 0
+                        f = 0
                         break
                     elif(answer == "yes"):
                         insert_mode = "yes"
@@ -96,6 +142,7 @@ def main():
             current_user = add_user()
             users.append(current_user)
             save_to_file(users)
+            recommend_exercise(current_user, exercises)
             break
 
         else:
